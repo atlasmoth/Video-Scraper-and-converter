82738873d9bc620717533167fccd4c8fc29e6995
atlasmoth <ebuka422@gmail.com>
2020-01-11 17:52:46 +0100

    second pass

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const puppeteer = require("puppeteer");
 
-async function scrape(link, converter) {
+async function scrape(...links) {
   const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(converter);
-  page.setDefaultTimeout(0);
-  page.on("dialog", async dialog => {
-    await dialog.accept(link);
-  });
-  await page.waitForSelector("#open_link");
-  await (await page.$("#open_link")).click();
+  const processor = async link => {
+    const page = await browser.newPage();
+    await page.goto("https://convert-video-online.com/");
+    page.setDefaultTimeout(0);
+    page.on("dialog", async dialog => {
+      await dialog.accept(link);
+    });
+    await page.waitForSelector("#open_link");
+    await (await page.$("#open_link")).click();
 
-  await page.waitForSelector(".file_details");
-  await (await page.$("#preset_dropdown")).click();
-  const [element] = await page.$x(`//*[@id="480p"]`);
-  await element.click();
+    await page.waitForSelector(".file_details");
+    await (await page.$("#preset_dropdown")).click();
+    const [element] = await page.$x(`//*[@id="360p"]`);
+    await element.click();
 
-  await (await page.$(".button_1.button_1_smaller")).click();
-  await page.waitForSelector(`#download_file_link[href*=".mp4"]`);
-  await (await page.$("#download_file_link")).click();
+    await (await page.$(".button_1.button_1_smaller")).click();
+    await page.waitForSelector(`#download_file_link[href*=".mp4"]`);
+    await (await page.$("#download_file_link")).click();
+  };
+  return Promise.all(links.map(item => processor(item)));
 }
