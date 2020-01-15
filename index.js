be32ea1f34b8255a5366ed32c0cd68c22e1018b8
atlasmoth <ebuka422@gmail.com>
2020-01-15 15:07:13 +0100

    more tweaks

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const puppeteer = require("puppeteer");
-
-async function scrape(...links) {
-  const browser = await puppeteer.launch({ headless: false });
+const path = require("path");
+const fs = require("fs");
+async function scrape(downloadPath, ...links) {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ["--window-size=1366,768", "--window-position=0,0"]
+  });
   const processor = async link => {
     const page = await browser.newPage();
-    await page.goto("https://convert-video-online.com/");
     page.setDefaultTimeout(0);
+    await page.goto("https://convert-video-online.com/");
+    await page._client.send("Page.setDownloadBehavior", {
+      behavior: "allow",
+      downloadPath: path.resolve(downloadPath)
+    });
+
     page.on("dialog", async dialog => {
       await dialog.accept(link);
     });
+
     await page.waitForSelector("#open_link");
     await (await page.$("#open_link")).click();
 
     await page.waitForSelector(".file_details");
     await (await page.$("#preset_dropdown")).click();
-    const [element] = await page.$x(`//*[@id="360p"]`);
+    const [element] = await page.$x(`//*[@id="480p"]`);
     await element.click();
 
     await (await page.$(".button_1.button_1_smaller")).click();
